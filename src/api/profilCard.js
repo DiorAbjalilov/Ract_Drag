5499515b1445970ce7ec81ba90dfea8d7591e952
DiorAbjalilov <abjalilovdiyor@gamil.com>
2021-10-05 14:20:02 +0500

    card news

--- src/api/profilCard.js
+++ src/api/profilCard.js
@@ -6,14 +6,30 @@ function CardProfil() {
   const [users, setUsers] = useState(null);
   useEffect(() => {
     load().then((result) => {
-      setUsers(result.data);
+      console.log(result.data[1].email);
+      setUsers(result.data[1].email);
     });
   }, []);
 
   return (
     <div>
       <Card id="card-1" className="card" draggable="true">
-        <p>card user</p>
+        <p>{users}</p>
+      </Card>
+      <Card id="card-2" className="card" draggable="true">
+        <p>{users}</p>
+      </Card>
+      <Card id="card-3" className="card" draggable="true">
+        <p>{users}</p>
+      </Card>
+      <Card id="card-4" className="card" draggable="true">
+        <p>{users}</p>
+      </Card>
+      <Card id="card-5" className="card" draggable="true">
+        <p>{users}</p>
+      </Card>
+      <Card id="card-6" className="card" draggable="true">
+        <p>{users}</p>
       </Card>
     </div>
   );
